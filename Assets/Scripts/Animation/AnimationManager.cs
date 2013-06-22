c0a225dd62899896bdad703c6d3eaa15b5cf480e
Cameron <cameron_9393@hotmail.com>
2013-06-22 18:22:11 +1000

    Further work on Animation Manager
    -Textures are now flipped automatically.
    -Animations are now set using setNewAnimation().
    -Fixed a bug in setting the next animation if the current animation is
    flagged as not looping.

--- Assets/Scripts/Animation/AnimationManager.cs
+++ Assets/Scripts/Animation/AnimationManager.cs
@@ -4,17 +4,34 @@ using System.Collections;
 public class AnimationManager : MonoBehaviour 
 {
 	public SpriteAnim[] animations; //The array of animations that are configured in the inspector
-	public string currentAnimation;
+	public string currentAnimation; //The currently playing animation
+	public bool flipTextureVertically = true; //Unity's UV system is flipped to how it's usually used. This means that the textures on planes are flipped vertically
 	
 	private float frameTime = 0; //The time that the frame has been active for
 	private uint currentFrame = 0; 
 	private float frameRate = 24.0f;
-	private int numFrames;
-	private int frameSkip;
-	private float currentTime;
-	private float nextFrameTime;
-	private float timeAllowance = 0.01f;
+	private int numFrames; //The number of frames in the current animation
+	private float currentTime; //The current time in the execution
+	private float nextFrameTime; //The time when the next frame is due
+	private float timeAllowance = 0.01f; //The time in seconds that the animation is allowed to deviate by (helps for lower framerates)
 	private bool currentAnimIsLooping;
+	
+	//Call this function to set the animation, don't alter currentAnimation directly
+	public void setNewAnimation(string newAnimation = "null")
+	{
+		//Set the frame back to 0 so the new animation starts from the start
+		//This also avoids the risk of going out of bounds if changing to an animation with a lower frame count
+		currentFrame = 0;
+		
+		if (newAnimation != "null")
+		{
+			currentAnimation = newAnimation;
+		}
+		else
+		{
+			Debug.Log("ERROR: No name given in AnimationManager.setNewAnimation()");
+		}
+	}
 	
 	// Use this for initialization
 	void Start () 
@@ -22,6 +39,7 @@ public class AnimationManager : MonoBehaviour
 		frameTime = 1.0f / frameRate;
 		currentTime = Time.time;
 		nextFrameTime = currentTime + frameTime;
+		if (flipTextureVertically) flipTex();
 	}
 	
 	// Update is called once per frame
@@ -35,11 +53,13 @@ public class AnimationManager : MonoBehaviour
 			nextFrameTime = currentTime + frameTime;
 			if (currentFrame > numFrames)
 			{
+				//If the animation is looping, set the current frame back to 0
 				if (isLooping())
 				{
 					currentFrame = 0;
 				}
 				
+				//Otherwise, find and play the animation specified in the inspector
 				else
 				{
 					currentFrame = 0;
@@ -47,24 +67,18 @@ public class AnimationManager : MonoBehaviour
 					{
 						if (animations[i].getName() == currentAnimation)
 						{
-							currentAnimation =  animations[i].getNextAnim();
+							setNewAnimation(animations[i].getNextAnim());
+							break;
 						}
 					}
 				}
 			}
-			//frameSkip= 0;
 		}
-		/*
-		else
-		{
-			frameSkip++;
-		}
-		*/
 		play (currentAnimation);
 	}
 	
 	
-	void play (string animName = "null", bool loop = false)
+	void play (string animName = "null")
 	{
 		//Abort if a name wasn't given for an animation
 		if (animName == "null")
@@ -72,15 +86,14 @@ public class AnimationManager : MonoBehaviour
 			Debug.Log("ERROR: No name given in AnimationManager.play()");
 			return;
 		}
-
+		
+		//Find the animation with the matching name and assign the current texture to the current frame
 		for (int i = 0; i < animations.Length; i++)
 		{
 			if (animations[i].getName() == animName)
 			{
 				getAnimData(i);
 				gameObject.renderer.material.mainTexture = animations[i].play(currentFrame);
-				
-				//Debug.Log(("animation"));
 			}
 		}
 	}
@@ -105,5 +118,11 @@ public class AnimationManager : MonoBehaviour
 		return false;
 	}
 	
-
+	void flipTex()
+	{
+		Vector2 offset = new Vector2(0,1);
+		Vector2 scale = new Vector2(1,-1);
+		gameObject.renderer.material.SetTextureOffset("_MainTex", offset);
+		gameObject.renderer.material.SetTextureScale("_MainTex", scale);
+	}
 }
